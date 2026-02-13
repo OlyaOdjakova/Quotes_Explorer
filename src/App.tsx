a7c226f769986a379e8d6264c163d77ebbd7659b
OlyaOdjakova <Olgica.Odzakova@students.finki.ukim.mk>
2026-02-13 21:37:13 +0100

    Install and setup the Chakra-UI library, remove not needed code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import MainView from "./main_view/MainView.tsx";
+import { Provider } from "@/components/ui/provider";
 
 function App() {
-  return <></>;
+  return (
+    <Provider>
+      <MainView />
+    </Provider>
+  );
 }
 
 export default App;
